ca6b19573032b7619accfa05e704198525842061
Jack Harper <jack@harper.id>
2019-12-16 08:54:06 +1000

    Set 7z permissions

--- deploy_server.sh
+++ deploy_server.sh
@@ -10,7 +10,8 @@ su hl2dm -c "mkdir ~/Steam && cd ~/Steam \
                 +app_update 232370 validate \
                 +quit \
         && cd ~ && curl -qLO \"https://github.com/jackharpr/hl2dm-xms/releases/download/v1.13/xms_pack_linux_v1.13.zip\" \
-        && unzip -o xms_pack_linux_v1.13.zip
+        && unzip -o xms_pack_linux_v1.13.zip \
+        && chmod +x hl2mp/addons/sourcemod/data/system2/linux/amd64/7z hl2mp/addons/sourcemod/data/system2/linux/i386/7z \
         && echo -e \"@ShutdownOnFailedCommand 1\n@NoPromptForPassword 1\nlogin anonymous\nforce_install_dir /home/hl2dm\napp_update 232370\nquit\" >> autoupdate \
         && echo \"screen -S hl2dm -d -m ./srcds_run -console -game hl2mp +map dm_lockdown +maxplayers 12 -tickrate 100 -autoupdate -steam_dir /home/hl2dm/Steam -steamcmd_script /home/hl2dm/autoupdate -strictportbind +port 27015 +tv_port 27020 +clientport 27005\" >> start_server.sh && chmod +x start_server.sh \
         && echo \"@reboot /bin/bash /home/hl2dm/start_server.sh\" >> cron && crontab cron && rm cron \
